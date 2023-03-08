0b6e3d69905c9ff55ce3f51278b0fef3a2633c89
Rene Saarsoo <nene@triin.net>
2023-03-08 12:20:33 +0200

    Format SQL using prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { SqlEditor } from "./SqlEditor";
+import { format } from "prettier";
+import * as plugin from "prettier-plugin-sql-cst";
+
+const exampleSql = `
+select supplier_name,city from
+(select * from suppliers join addresses on suppliers.address_id=addresses.id)
+as suppliers
+where supplier_id>500
+order by supplier_name asc,city desc;
+`;
+
+function formatSql(sql: string): string {
+  return format(sql, {
+    parser: "sqlite",
+    plugins: [plugin],
+  });
+}
 
 export function App() {
+  const [sql, setSql] = useState(exampleSql);
+
   return (
     <div className="App">
-      <SqlEditor value="SELECT * FROM my_table WHERE col > 10" />
-      <SqlEditor value={"SELECT\n1,\n2,\n3,\n4"} />
+      <SqlEditor value={sql} onChange={setSql} />
+      <SqlEditor value={formatSql(sql)} />
     </div>
-  )
+  );
 }
