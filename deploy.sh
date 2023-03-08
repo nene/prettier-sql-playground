ef48956e10039b258e1673c1da971d2698e88add
Rene Saarsoo <nene@triin.net>
2023-03-08 13:55:06 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nene/prettier-sql-playground.git main:gh-pages
+
+cd -
